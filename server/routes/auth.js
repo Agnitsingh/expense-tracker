89bfa8796ad4f3e179669501f7019e96f69b828b
Agnit Singh <agnit38@gmail.com>
2024-12-09 20:14:38 +0530

    Final auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-// Register new user
 router.post('/register', userController.register);
 
-// Login user
 router.post('/login', userController.login);
 
 module.exports = router; 
