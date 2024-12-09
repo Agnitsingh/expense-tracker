9ce19a1cff8e5e5c09a4d2738922586049c6699d
Agnit Singh <agnit38@gmail.com>
2024-12-09 19:59:23 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const connectDB = require('./db');
 const expenseController = require('./controllers/expense.controller');
 const authRoutes = require('./routes/auth');
 
-// Middleware to extract user ID from headers
 const extractUserId = (req, res, next) => {
     const userId = req.headers['user-id'];
     if (!userId) {
@@ -14,16 +13,12 @@ const extractUserId = (req, res, next) => {
 };
 
 exports.addMiddlewares = function(app) {
-    // Connect to MongoDB
     connectDB().catch(console.error);
 
-    // Middleware
     app.use(express.json());
 
-    // Auth routes
     app.use('/api/auth', authRoutes);
 
-    // Expense routes with user ID extraction
     app.get('/api/expenses', extractUserId, expenseController.getExpenses);
     app.post('/api/expenses', extractUserId, expenseController.createExpense);
     app.put('/api/expenses/:id', extractUserId, expenseController.updateExpense);
