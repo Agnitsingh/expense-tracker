91232c97549340d63860b5a27d685d20037a4034
Agnit Singh <agnit38@gmail.com>
2024-12-09 20:14:05 +0530

    Final user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,16 +1,13 @@
 const User = require('./user.model');
 const bcrypt = require('bcrypt');
 
-// Number of salt rounds for bcrypt
 const SALT_ROUNDS = 10;
 
-// Email validation function
 const isValidEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 };
 
-// Password validation function
 const isValidPassword = (password) => {
     return password && password.length >= 6;
 };
@@ -19,21 +16,18 @@ exports.register = async (req, res) => {
     try {
         const { email, password, name } = req.body;
 
-        // Validate email
         if (!isValidEmail(email)) {
             return res.status(400).json({ 
                 message: 'Please enter a valid email address'
             });
         }
 
-        // Validate password
         if (!isValidPassword(password)) {
             return res.status(400).json({ 
                 message: 'Password must be at least 6 characters long'
             });
         }
 
-        // Check if user already exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ 
@@ -41,10 +35,10 @@ exports.register = async (req, res) => {
             });
         }
 
-        // Hash password
+        
         const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-        // Create new user with hashed password
+       
         const user = new User({
             email,
             password: hashedPassword,
@@ -71,14 +65,14 @@ exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        // Validate email format
+       
         if (!isValidEmail(email)) {
             return res.status(400).json({ 
                 message: 'Please enter a valid email address'
             });
         }
 
-        // Find user
+        
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({ 
@@ -86,7 +80,7 @@ exports.login = async (req, res) => {
             });
         }
 
-        // Compare password with hashed password in database
+        
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             return res.status(401).json({ 
